102c07b67c5cec44371c75518b7abafdb307ae91
Shilpa Shaw <135813624+ShilpaShaw17@users.noreply.github.com>
2024-05-31 02:31:26 +0530

    Update Music_Store_Query.sql

--- Music_Store_Query.sql
+++ Music_Store_Query.sql
@@ -2,10 +2,10 @@
 
 /* Q1: Who is the senior most employee based on job title? */
 
-SELECT title, last_name, first_name 
+SELECT top 1 title, last_name, first_name 
 FROM employee
 ORDER BY levels DESC
-LIMIT 1
+
 
 
 /* Q2: Which countries have the most Invoices? */
@@ -27,22 +27,22 @@ ORDER BY total DESC
 Write a query that returns one city that has the highest sum of invoice totals. 
 Return both the city name & sum of all invoice totals */
 
-SELECT billing_city,SUM(total) AS InvoiceTotal
+SELECT top 1 billing_city,SUM(total) AS InvoiceTotal
 FROM invoice
 GROUP BY billing_city
 ORDER BY InvoiceTotal DESC
-LIMIT 1;
+
 
 
 /* Q5: Who is the best customer? The customer who has spent the most money will be declared the best customer. 
 Write a query that returns the person who has spent the most money.*/
 
-SELECT customer.customer_id, first_name, last_name, SUM(total) AS total_spending
+SELECT top 1 customer.customer_id, first_name, last_name, SUM(total) AS total_spending
 FROM customer
 JOIN invoice ON customer.customer_id = invoice.customer_id
 GROUP BY customer.customer_id
 ORDER BY total_spending DESC
-LIMIT 1;
+
 
 
 
@@ -81,7 +81,7 @@ ORDER BY email;
 /* Q2: Let's invite the artists who have written the most rock music in our dataset. 
 Write a query that returns the Artist name and total track count of the top 10 rock bands. */
 
-SELECT artist.artist_id, artist.name,COUNT(artist.artist_id) AS number_of_songs
+SELECT  top 10 artist.artist_id, artist.name,COUNT(artist.artist_id) AS number_of_songs
 FROM track
 JOIN album ON album.album_id = track.album_id
 JOIN artist ON artist.artist_id = album.artist_id
@@ -89,7 +89,7 @@ JOIN genre ON genre.genre_id = track.genre_id
 WHERE genre.name LIKE 'Rock'
 GROUP BY artist.artist_id
 ORDER BY number_of_songs DESC
-LIMIT 10;
+;
 
 
 /* Q3: Return all the track names that have a song length longer than the average song length. 
@@ -116,14 +116,14 @@ so you need to use the InvoiceLine table to find out how many of each product wa
 for each artist. */
 
 WITH best_selling_artist AS (
-	SELECT artist.artist_id AS artist_id, artist.name AS artist_name, SUM(invoice_line.unit_price*invoice_line.quantity) AS total_sales
+	SELECT top 1 artist.artist_id AS artist_id, artist.name AS artist_name, SUM(invoice_line.unit_price*invoice_line.quantity) AS total_sales
 	FROM invoice_line
 	JOIN track ON track.track_id = invoice_line.track_id
 	JOIN album ON album.album_id = track.album_id
 	JOIN artist ON artist.artist_id = album.artist_id
 	GROUP BY 1
 	ORDER BY 3 DESC
-	LIMIT 1
+	
 )
 SELECT c.customer_id, c.first_name, c.last_name, bsa.artist_name, SUM(il.unit_price*il.quantity) AS amount_spent
 FROM invoice i
@@ -161,26 +161,26 @@ SELECT * FROM popular_genre WHERE RowNo <= 1
 
 /* Method 2: : Using Recursive */
 
-WITH RECURSIVE
-	sales_per_country AS(
-		SELECT COUNT(*) AS purchases_per_genre, customer.country, genre.name, genre.genre_id
-		FROM invoice_line
-		JOIN invoice ON invoice.invoice_id = invoice_line.invoice_id
-		JOIN customer ON customer.customer_id = invoice.customer_id
-		JOIN track ON track.track_id = invoice_line.track_id
-		JOIN genre ON genre.genre_id = track.genre_id
-		GROUP BY 2,3,4
-		ORDER BY 2
-	),
-	max_genre_per_country AS (SELECT MAX(purchases_per_genre) AS max_genre_number, country
-		FROM sales_per_country
-		GROUP BY 2
-		ORDER BY 2)
+WITH sales_per_country AS (
+    SELECT COUNT(*) AS purchases_per_genre, customer.country, genre.name, genre.genre_id
+    FROM invoice_line
+    JOIN invoice ON invoice.invoice_id = invoice_line.invoice_id
+    JOIN customer ON customer.customer_id = invoice.customer_id
+    JOIN track ON track.track_id = invoice_line.track_id
+    JOIN genre ON genre.genre_id = track.genre_id
+    GROUP BY customer.country, genre.name, genre.genre_id
+),
+max_genre_per_country AS (
+    SELECT MAX(purchases_per_genre) AS max_genre_number, country
+    FROM sales_per_country
+    GROUP BY country
+)
 
 SELECT sales_per_country.* 
 FROM sales_per_country
 JOIN max_genre_per_country ON sales_per_country.country = max_genre_per_country.country
 WHERE sales_per_country.purchases_per_genre = max_genre_per_country.max_genre_number;
+
 
 
 /* Q3: Write a query that determines the customer that has spent the most on music for each country. 
@@ -204,25 +204,40 @@ SELECT * FROM Customter_with_country WHERE RowNo <= 1
 
 /* Method 2: Using Recursive */
 
-WITH RECURSIVE 
-	customter_with_country AS (
-		SELECT customer.customer_id,first_name,last_name,billing_country,SUM(total) AS total_spending
-		FROM invoice
-		JOIN customer ON customer.customer_id = invoice.customer_id
-		GROUP BY 1,2,3,4
-		ORDER BY 2,3 DESC),
+WITH customer_with_country AS (
+    SELECT 
+        customer.customer_id,
+        first_name,
+        last_name,
+        billing_country,
+        SUM(total) AS total_spending
+    FROM invoice
+    JOIN customer ON customer.customer_id = invoice.customer_id
+    GROUP BY 
+        customer.customer_id,
+        first_name,
+        last_name,
+        billing_country
+),
 
-	country_max_spending AS(
-		SELECT billing_country,MAX(total_spending) AS max_spending
-		FROM customter_with_country
-		GROUP BY billing_country)
+country_max_spending AS (
+    SELECT 
+        billing_country,
+        MAX(total_spending) AS max_spending
+    FROM customer_with_country
+    GROUP BY billing_country
+)
 
-SELECT cc.billing_country, cc.total_spending, cc.first_name, cc.last_name, cc.customer_id
-FROM customter_with_country cc
-JOIN country_max_spending ms
-ON cc.billing_country = ms.billing_country
+SELECT 
+    cc.billing_country, 
+    cc.total_spending, 
+    cc.first_name, 
+    cc.last_name, 
+    cc.customer_id
+FROM customer_with_country cc
+JOIN country_max_spending ms ON cc.billing_country = ms.billing_country
 WHERE cc.total_spending = ms.max_spending
-ORDER BY 1;
+ORDER BY cc.billing_country;
 
 
 /* source: www.youtube.com/@RishabhMishraOfficial */
